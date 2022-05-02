0f492d1aaa0bfc3b8f15f78b2318a01831ba91e0
Rahim2725 <info.abdurrahim12@gmail.com>
2022-05-02 20:49:43 +0600

    nav bar addded

---
+++ src/pages/Home/Home/Home.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Header from '../../Shared/Header/Header';
+
+const Home = () => {
+    return (
+        <>
+            <Header></Header>
+        </>
+    );
+};
+
+export default Home;
---
+++ src/pages/Shared/Header/Header.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { Container, Nav, Navbar } from 'react-bootstrap';
+import { Link } from 'react-router-dom';
+import logo from '../../../Images/logo.png'
+import './Header.css'
+
+const Header = () => {
+    return (
+        <div>
+            <Navbar collapseOnSelect expand="lg" className="navbar" >
+                <Container>
+                    <Navbar.Brand as={Link} to="/" >
+                        <img height="30" src={logo} alt="" />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav">
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                        </Nav>
+                        <Nav>
+                            <Nav.Link as={Link} to="/about">About us</Nav.Link>
+                            <Nav.Link as={Link} to="/login">
+                                Login
+                            </Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        </div>
+    );
+};
+
+export default Header;
